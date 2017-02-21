a5d9f7b3399299cab00a2e0372c6a9a7ca5eec5b
Ola Klingberg <ola@olaklingberg.com>
2017-02-21 14:01:41 -0500

    Adds map with a marker with the users location. + Adds bootswatch themes.

--- src/app/riders-map/riders-map.component.ts
+++ src/app/riders-map/riders-map.component.ts
@@ -4,34 +4,47 @@ import { marker } from "../../interfaces/marker";
 @Component({
   selector: 'ride-riders-map',
   templateUrl: './riders-map.component.html',
-  styleUrls: ['./riders-map.component.scss']
+  styleUrls: [ './riders-map.component.scss' ]
 })
 export class RidersMapComponent implements OnInit {
   // Zoom level
-  zoom: number = 14;
+  zoom: number = 16;
+
   // Start position
   lat: number = 40.742706;
   lng: number = -73.998786;
+
   // Values
   markerName: string;
   markerLat: string;
   markerLng: string;
   markerDraggable: string;
+
   // Markers
   newMarker: marker;
   markers: marker[] = [];
+  url: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.newMarker = {
-      name: 'Ola',
-      lat: 40.742706,
-      lng: -73.998786,
-      draggable: true
-    };
+    navigator.geolocation.getCurrentPosition(position => {
 
-    this.markers.push(this.newMarker);
+      this.newMarker = {
+        name: 'Ola',
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        draggable: true
+      };
+
+      this.markers.push(this.newMarker);
+
+
+
+
+    });
+
   }
 
 }

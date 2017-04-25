9bb0ddb2b86b23b63c1cc6f627536f99bff3111f
Ola Klingberg <ola@olaklingberg.com>
2017-04-25 14:31:23 -0400

    Commit before desperate attempt to downgrade to Angular 2, to make Heroku deployment work.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // api: "https://ride-planner-backend2.herokuapp.com",
-  api: "http://localhost:3051",
+  api: "https://ride-planner-backend2.herokuapp.com",
   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
   dummyCoords: false
 };

c37e5828bae7a29acaf26d1f900ed9ca0efbc005
Ola Klingberg <ola@olaklingberg.com>
2017-05-04 15:59:22 -0400

    Accepts defeat. Heroku env variables can't be pulled into angular-cli.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   api: "https://ride-planner-backend.herokuapp.com",
   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
-  dummyCoords: process.env.DUMMY_COORDS,
-  dummyMovement: process.env.DUMMY_MOVEMENT
+  dummyCoords: true,
+  dummyMovement: true,
 };

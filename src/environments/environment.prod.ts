6238ffb46ecae037f1b4d0b24bb43bd04b003664
Ola Klingberg <ola@olaklingberg.com>
2017-05-19 13:41:01 -0400

    Testing.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api: "https://ride-planner-backend.herokuapp.com",
   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
-  dummyCoords: true,
-  dummyMovement: true,
+  dummyCoords: false,
+  dummyMovement: false,
   storage: sessionStorage
 };

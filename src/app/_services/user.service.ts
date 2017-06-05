2793e3d8b634a5045ddb90d20d38ed67f6ca27ac
Ola Klingberg <ola@olaklingberg.com>
2017-06-05 09:01:56 -0400

    Should force all requests to https.

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -48,7 +48,9 @@ export class UserService {
     this.geoWatch = navigator.geolocation.watchPosition(position => {
           console.log("position:", position);
           this.position$.next(position);
-          this.debuggingService.debugMessages$.next(`${this.user$.value.fname} ${this.user$.value.lname}. geolocation.watchPosition: success: Latitude: ${position.coords.latitude}.`);
+          let user = this.user$.value;
+          if (!user || !user.fname) this.debuggingService.debugMessages$.next(`geolocation.watchPosition: success: Latitude: ${position.coords.latitude}.`);
+          if (user && user.fname) this.debuggingService.debugMessages$.next(`${this.user$.value.fname} ${this.user$.value.lname}. geolocation.watchPosition: success: Latitude: ${position.coords.latitude}.`);
           // Set timer to up rerun watchPosition if it has not yielded results for a while. Logically, this should not be needed, but it often seems to yield a new position.
           clearTimeout(this.geoWatchTimer);
           this.startGeoWatchTimer(position);

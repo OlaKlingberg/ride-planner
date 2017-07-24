bb3e5565ac9180d8a57a932b5eaff9b8d0cd2d76
Ola Klingberg <ola@olaklingberg.com>
2017-07-24 15:08:25 -0400

    Trouble shooting.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   api: "https://ride-planner-backend.herokuapp.com",
+  // api: "http://localhost:3051",
   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
   dummyPosition: false,
   dummyMovement: false,

1fd73bd147bec6af3bc8c725b3564c2f89fd9778
Ola Klingberg <ola@olaklingberg.com>
2017-09-19 14:42:43 -0400

    Guides user in entering phone number on registration page.

--- src/app/authentication/registration/registration.component.ts
+++ src/app/authentication/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { Router } from "@angular/router";
 
 import { AlertService } from "../../alert/alert.service";
@@ -17,10 +17,45 @@ export class RegistrationComponent implements OnDestroy {
 
   private subscription: Subscription;
 
+  @ViewChild('phoneField') phoneField: ElementRef;
+  @ViewChild('emergencyPhoneField') emergencyPhoneField: ElementRef;
+
   constructor(private router: Router,
               private userService: UserService,
               private alertService: AlertService) {
   }
+
+  formatPhone($event, field) {
+    this.model[field] += '.';
+
+    setTimeout(() => {
+      // Limit length to 10 digits plus 2 hyphens.
+      let ph = $event.substr(0, 12);
+
+      // Remove all non-number characters.
+      ph = ph.replace(/\D/g, '');
+      let length = ph.length;
+
+      // Add spaces to end so string becomes 10 characters long.
+      ph += Array(11 - length).join(' ');
+
+      // (Re-)insert hyphens into the string.
+      ph = `${ph.substr(0, 3)}-${ph.substr(3, 3)}-${ph.substr(6)}`;
+
+      this.model[field] = ph;
+
+      // Set cursor position.
+      let pos = length;
+      if ( length >= 3 ) pos++;
+      if ( length >= 6 ) pos++;
+      // If the user deleted a hyphen, put the cursor to the left of the hyphen.
+      if ($event.length > 1 && $event.split('-').length !== 3) pos--;
+      setTimeout(() => {
+        this[field + 'Field'].nativeElement.setSelectionRange(pos, pos);
+      }, 0);
+    });
+  }
+
 
   register() {
     this.loading = true;
@@ -37,6 +72,6 @@ export class RegistrationComponent implements OnDestroy {
   }
 
   ngOnDestroy() {
-    if (this.subscription) this.subscription.unsubscribe();
+    if ( this.subscription ) this.subscription.unsubscribe();
   }
 }

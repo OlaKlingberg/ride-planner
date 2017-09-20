05b97017c9c165577b935f55a63a8f6d891956e4
Ola Klingberg <ola@olaklingberg.com>
2017-09-20 10:57:29 -0400

    Unsubscribes from subscriptions in members and riders.

--- src/app/rider/rider-list/rider-list.component.ts
+++ src/app/rider/rider-list/rider-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MdSort } from '@angular/material';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
@@ -8,17 +8,20 @@ import { RideSubjectService } from '../../ride/ride-subject.service';
 import { Observable } from 'rxjs/Observable';
 import { RiderService } from '../rider.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   templateUrl: './rider-list.component.html',
   styleUrls: [ './rider-list.component.scss' ]
 })
-export class RiderListComponent implements OnInit {
+export class RiderListComponent implements OnInit, OnDestroy {
   displayedColumns = [ 'fullName', 'phone', 'emergencyName', 'emergencyPhone', 'disconnected' ];
   dataSource: RiderListDataSource | null;
   modalRef: BsModalRef;
   ride: string = '';
   rider: any;
+
+  private subscription: Subscription;
 
   @ViewChild('filter') filter: ElementRef = null;
   @ViewChild(MdSort) sort: MdSort;
@@ -37,14 +40,16 @@ export class RiderListComponent implements OnInit {
     // Todo: It seems wrong to have to include this.userService in this method call. How do I get rid of that?
     this.dataSource = new RiderListDataSource(this.sort, this.riderService);
 
-    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+    const keyup$ = Observable.fromEvent(this.filter.nativeElement, 'keyup')
         .debounceTime(150)
-        .distinctUntilChanged()
-        .subscribe(() => {
+        .distinctUntilChanged();
+
+    this.subscription = keyup$.subscribe(() => {
           if ( !this.dataSource ) return;
           this.dataSource.filter = this.filter.nativeElement.value;
         });
 
+    // Todo: Refactor this so that I can removeEventListeners OnDestroy.
     window.addEventListener('resize', () => {
       this.displayColumns();
     });
@@ -64,5 +69,8 @@ export class RiderListComponent implements OnInit {
     this.modalRef = this.modalService.show(template);
   }
 
+  ngOnDestroy() {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
 
 }
--- src/app/user/member-list/member-list.component.ts
+++ src/app/user/member-list/member-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MdSort } from '@angular/material';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
@@ -7,6 +7,7 @@ import { MemberListDataSource } from './member-list-datasource';
 import { UserService } from '../user.service';
 import { Observable } from 'rxjs/Observable';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   templateUrl: './member-list.component.html',
@@ -17,6 +18,8 @@ export class MemberListComponent implements OnInit {
   dataSource: MemberListDataSource | null;
   member: any;
   modalRef: BsModalRef;
+
+  private subscription: Subscription;
 
   @ViewChild('filter') filter: ElementRef;
   @ViewChild(MdSort) sort: MdSort;
@@ -30,14 +33,16 @@ export class MemberListComponent implements OnInit {
     // Todo: It seems wrong to have to include this.userService in this method call. How do I get rid of that?
     this.dataSource = new MemberListDataSource(this.sort, this.userService);
 
-    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+    const keyup$ = Observable.fromEvent(this.filter.nativeElement, 'keyup')
         .debounceTime(150)
-        .distinctUntilChanged()
-        .subscribe(() => {
+        .distinctUntilChanged();
+
+    this.subscription = keyup$.subscribe(() => {
           if (!this.dataSource) return;
           this.dataSource.filter = this.filter.nativeElement.value;
         });
 
+    // Todo: Refactor this so that I can removeEventListeners OnDestroy.
     window.addEventListener('resize', () => {
       this.displayColumns();
     });
@@ -57,5 +62,8 @@ export class MemberListComponent implements OnInit {
     this.modalRef = this.modalService.show(template);
   }
 
+  ngOnDestroy() {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
 
 }

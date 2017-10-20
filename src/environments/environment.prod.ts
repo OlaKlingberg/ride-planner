32347952c5b5c2df8f78be5ec4652d73fd8a6b07
Ola Klingberg <ola@olaklingberg.com>
2017-10-20 12:38:55 -0400

    Sets variables for live deployment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: "https://ride-planner-backend.herokuapp.com",
-  demoMode: true,
+  demoMode: false,
   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
   production: true,
   storage: sessionStorage

dee8f478187e9fdffdd54bed4d40efed1fcd560c
Ola Klingberg <ola@olaklingberg.com>
2017-10-20 14:10:39 -0400

    Removes MdFormFieldModule import in RideModule.

--- src/app/rider/rider.module.ts
+++ src/app/rider/rider.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { RiderListComponent } from './rider-list/rider-list.component';
 import { RiderRoutingModule } from './rider-routing.module';
-import { MdFormFieldModule, MdInputModule, MdSortModule, MdTableModule, MdTooltipModule } from '@angular/material';
+import {
+  // MdFormFieldModule,
+  MdInputModule,
+  MdSortModule,
+  MdTableModule,
+  MdTooltipModule
+} from '@angular/material';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MdFormFieldModule,
+    // MdFormFieldModule,
     MdInputModule,
     MdSortModule,
     MdTableModule,

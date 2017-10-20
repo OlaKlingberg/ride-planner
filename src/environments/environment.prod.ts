df8435fc8d3a2e0601125cb5c80b50c4535409ef
Ola Klingberg <ola@olaklingberg.com>
2017-10-20 12:54:48 -0400

    Working with environment variable setup.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,16 @@
+// Master
 export const environment = {
   api: "https://ride-planner-backend.herokuapp.com",
   demoMode: false,
   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
   production: true,
 };
+
+// Demo
+// export const environment = {
+//   api: "https://ride-planner-demo-backend.herokuapp.com",
+//   demoMode: true,
+//   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
+//   production: true,
+// };
+

296297ff8364673c8a328d7c705add9ef9db695d
Ola Klingberg <ola@olaklingberg.com>
2017-10-23 14:28:20 -0400

    Adds environment.untracked and environment.prod.untracked.

---
+++ src/environments/environment.untracked.ts
@@ -0,0 +1,20 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
+// Live
+// export const environment = {
+//   api: "http://localhost:3051",
+//   demoMode: false,
+//   googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
+//   production: false
+// };
+
+// Demo
+export const environment = {
+  api: "http://localhost:3051",
+  demoMode: true,
+  googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
+  production: false
+};

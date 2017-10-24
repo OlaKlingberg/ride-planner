b46b2b7fa2732ee4827a5e24b9bce78bdb489966
Ola Klingberg <ola@olaklingberg.com>
2017-10-24 16:54:21 -0400

    Ends attempts to import Heroku config vars. Resets everything. Adds separate googleMapsKeys for local and remote.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,15 @@
+// Live
+// export const environment = {
+//   api: "http://localhost:3051",
+//   demoMode: false,
+//   googleMapsKey: "AIzaSyA9n1FB51ManxDOvn4cYd8qqoC1vgS8Pl8",
+//   production: false
+// };
+
 // Demo
 export const environment = {
   api: "http://localhost:3051",
   demoMode: true,
-  googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
-  production: true
+  googleMapsKey: "AIzaSyA9n1FB51ManxDOvn4cYd8qqoC1vgS8Pl8",
+  production: false
 };

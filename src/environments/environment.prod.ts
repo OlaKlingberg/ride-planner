918825fcd1754abc129a36679d37b03e6b2b3e9d
Ola Klingberg <ola@olaklingberg.com>
2017-10-24 14:36:52 -0400

    Troubleshooting.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,8 +10,8 @@
 export const environment = {
   api: "https://ride-planner-demo-backend.herokuapp.com",
   demoMode: true,
-  // googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
-  googleMapsKey: process.env.googleMapKey,
+  googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
+  // googleMapsKey: process.env.googleMapKey,
   production: true
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   api: "http://localhost:3051",
   demoMode: true,
-  // googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
-  googleMapsKey: process.env.googleMapKey,
+  googleMapsKey: "AIzaSyDcbNgBS0ykcFj8em8xT5WcDHZbFiVL5Ok",
+  // googleMapsKey: process.env.googleMapKey,
   production: true
 };

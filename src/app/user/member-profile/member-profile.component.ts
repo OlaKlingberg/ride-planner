db1881b2bb4ed266f9cf732278eb38dfd31fe92b
Ola Klingberg <ola.klingberg@gmail.com>
2020-03-24 15:37:45 -0400

    Adds ability for superAdmin to turn members into demo users.

--- src/app/user/member-profile/member-profile.component.ts
+++ src/app/user/member-profile/member-profile.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from '../user';
 import { UserService } from '../user.service';
 import { Subscription } from 'rxjs/Subscription';
+import { environment } from '../../../environments/environment';
 
 @Component({
   templateUrl: './member-profile.component.html',
@@ -11,6 +12,7 @@ import { Subscription } from 'rxjs/Subscription';
 export class MemberProfileComponent implements OnInit, OnDestroy {
   loading: boolean = false;
   member: User;
+  superAdmin = environment.superAdmin;
   user: User;
   fname: string;
 
